7286a3f0c7009e9e1d96a8869daf11c4f97f6c43
Johan Lindqvist <johan.lindqvist@forumvirium.fi>
2019-12-03 16:48:59 +0200

    Refined courier app models; timestamps, phone numbers & misc corrections.

--- django_server/fvh_courier/models.py
+++ django_server/fvh_courier/models.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.models import User
-from django.contrib.gis.db.models import PointField
+# from django.contrib.gis.db.models import PointField
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class Address(models.Model):
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Address(TimestampedModel):
     street_address = models.CharField(verbose_name=_('street address'), max_length=128)
     postal_code = models.CharField(verbose_name=_('postal code'), max_length=16)
     city = models.CharField(verbose_name=_('city'), max_length=64)
@@ -13,16 +21,18 @@ class Address(models.Model):
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     lon = models.DecimalField(max_digits=9, decimal_places=6)
 
-    coordinate = PointField()
+    # coordinate = PointField()
 
     class Meta:
         verbose_name = _('address')
         verbose_name_plural = _('addresses')
 
 
-class Package(models.Model):
-    pickup_at = models.ForeignKey(Address, verbose_name=_('pickup location'), related_name='outbound_packages')
-    deliver_to = models.ForeignKey(Address, verbose_name=_('destination'), related_name='inbound_packages')
+class Package(TimestampedModel):
+    pickup_at = models.ForeignKey(Address, verbose_name=_('pickup location'), related_name='outbound_packages',
+                                  on_delete=models.PROTECT)
+    deliver_to = models.ForeignKey(Address, verbose_name=_('destination'), related_name='inbound_packages',
+                                   on_delete=models.PROTECT)
 
     height = models.PositiveIntegerField(verbose_name=_('height'), help_text=_('in cm'))
     width = models.PositiveIntegerField(verbose_name=_('width'), help_text=_('in cm'))
@@ -30,22 +40,40 @@ class Package(models.Model):
 
     weight = models.DecimalField(verbose_name=_('weight'), help_text=_('in kg'), max_digits=7, decimal_places=2)
 
-    sender = models.ForeignKey(User, verbose_name=_('sender'), related_name='packages')
+    sender = models.ForeignKey(User, verbose_name=_('sender'), related_name='sent_packages', on_delete=models.PROTECT)
 
     recipient = models.CharField(max_length=128, verbose_name=_('recipient'))
     recipient_phone = models.CharField(max_length=32, verbose_name=_('recipient phone number'))
 
-    courier = models.ForeignKey(User, verbose_name=_('courier'), null=True, blank=True, related_name='packages')
+    courier = models.ForeignKey(User, verbose_name=_('courier'), null=True, blank=True, related_name='delivered_packages',
+                                on_delete=models.PROTECT)
 
-    earliest_pickup_time = models.TimeField(verbose_name=_('earliest pickup time'))
-    latest_pickup_time = models.TimeField(verbose_name=_('latest pickup time'))
+    earliest_pickup_time = models.DateTimeField(verbose_name=_('earliest pickup time'))
+    latest_pickup_time = models.DateTimeField(verbose_name=_('latest pickup time'))
 
-    earliest_delivery_time = models.TimeField(verbose_name=_('earliest delivery time'))
-    latest_delivery_time = models.TimeField(verbose_name=_('latest delivery time'))
+    earliest_delivery_time = models.DateTimeField(verbose_name=_('earliest delivery time'))
+    latest_delivery_time = models.DateTimeField(verbose_name=_('latest delivery time'))
 
-    picked_up_time = models.TimeField(verbose_name=_('picked up at'), blank=True, null=True)
-    delivered_time = models.TimeField(verbose_name=_('delivered at'), blank=True, null=True)
+    picked_up_time = models.DateTimeField(verbose_name=_('picked up at'), blank=True, null=True)
+    delivered_time = models.DateTimeField(verbose_name=_('delivered at'), blank=True, null=True)
 
     class Meta:
         verbose_name = _('package')
         verbose_name_plural = _('packages')
+        ordering = ['-earliest_pickup_time']
+
+    @classmethod
+    def available_packages(cls):
+        """
+        Return all packages for which delivery has been requested but which are not yet reserved by any courier.
+        """
+        return cls.objects.filter(courier__isnull=True)
+
+
+class PhoneNumber(TimestampedModel):
+    user = models.ForeignKey(User, related_name='phone_numbers', on_delete=models.CASCADE)
+    number = models.CharField(max_length=32, verbose_name=_('phone number'))
+
+    class Meta:
+        verbose_name = _('phone number')
+        verbose_name_plural = _('phone numbers')
