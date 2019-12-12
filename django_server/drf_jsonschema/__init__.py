6d6e23c03bfe0ad91021ad80404d66356194ccdf
Johan Lindqvist <johan.lindqvist@forumvirium.fi>
2019-12-12 10:50:06 +0200

    Refs #7 - Add 3rd party package drf_jsonschema and a jsonschema action for outgoing packages to support package creation form generation in UI

---
+++ django_server/drf_jsonschema/__init__.py
@@ -0,0 +1,5 @@
+from .fields import JSONSchemaField, SerializerJSONField  # noqa
+from .convert import to_jsonschema  # noqa
+
+# import the converters to register them
+from . import converters  # noqa
