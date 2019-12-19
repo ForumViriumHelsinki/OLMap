fb6bc324efdfa89a8dee3da03911bd102093ae37
Johan Lindqvist <johan.lindqvist@forumvirium.fi>
2019-12-19 14:42:16 +0200

    Refs #17 - Set up demo instance with Docker:  - Use same names for db settings in Django & Postgres containers so they are defined only once  - Add entrypoint.sh to wait for db & migrate as needed  - Add init_docker.sh to avoid having to remember anything

--- django_server/city_logistics/settings.py
+++ django_server/city_logistics/settings.py
@@ -83,9 +83,9 @@ WSGI_APPLICATION = 'city_logistics.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "NAME": os.environ.get("POSTGRES_DB", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("POSTGRES_USER", "user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
