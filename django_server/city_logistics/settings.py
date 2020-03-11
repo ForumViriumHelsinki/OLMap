97701d5fef4ea0678187c44c0335b78375163acf
Johan Lindqvist <johan.lindqvist@forumvirium.fi>
2020-03-11 15:24:24 +0200

    Refs #51 - Add allauth.socialaccount to enable deleting users

--- django_server/city_logistics/settings.py
+++ django_server/city_logistics/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
     'rest_auth',
     'rest_auth.registration',
     'city_logistics.apps.AdminConfig',
