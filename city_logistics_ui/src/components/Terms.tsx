94d7c83fc840aa9fcc73f3fddefad189b20fc468
Johan Lindqvist <johan.lindqvist@forumvirium.fi>
2020-04-27 09:48:49 +0300

    Refs #85 - Add CC0 license to OLMap terms

--- city_logistics_ui/src/components/Terms.tsx
+++ city_logistics_ui/src/components/Terms.tsx
@@ -3,7 +3,7 @@ import Modal from "util_components/Modal";
 
 type TermsProps = {}
 
-type TermsState = {showTerms: boolean}
+type TermsState = {showTerms: any}
 
 const initialState: TermsState = {showTerms: false};
 
@@ -15,13 +15,15 @@ export default class Terms extends React.Component<TermsProps, TermsState> {
     return <>
       <p>
         By using this website, you agree to
-        our <a onClick={() => this.setState({showTerms: true})} className="clickable text-primary">
+        our <a onClick={() => this.setState({showTerms: 'terms'})} className="clickable text-primary">
           Usage terms & privacy policy
-        </a>.
+        </a>, and that any map notes and images published here are placed in the public domain under
+        the <a onClick={() => this.setState({showTerms: 'cc0'})} className="clickable text-primary">CC0 License</a>.
       </p>
       {showTerms &&
         <Modal onClose={() => this.setState({showTerms: false})} title="">
-          <iframe style={{height: 'calc(100vh - 270px)', width: '100%', border: 'none'}} className="mt-2" src="/terms.html"/>
+          <iframe style={{height: 'calc(100vh - 270px)', width: '100%', border: 'none'}}
+                  className="mt-2" src={`/${showTerms}.html`}/>
           <div className="p-2">
             <button className="btn btn-outline-primary btn-block"
                     onClick={() => this.setState({showTerms: false})}>
