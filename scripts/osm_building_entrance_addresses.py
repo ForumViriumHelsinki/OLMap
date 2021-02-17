f462fd29f1b50ea18dc4758cac8bb9ae5745d4e9
Johan Lindqvist <johan.lindqvist@forumvirium.fi>
2021-02-17 17:38:51 +0200

    Sort buildings in osm_building_entrance_addresses: prioritize buildings with multiple street addresses with the same street name

--- scripts/osm_building_entrance_addresses.py
+++ scripts/osm_building_entrance_addresses.py
@@ -1,3 +1,5 @@
+import re
+
 import overpy, json
 
 api = overpy.Overpass()
@@ -15,18 +17,33 @@ result = api.query(query)
 
 entrance_index = dict((n.id, n) for n in result.nodes)
 
+
 def to_address(tags):
     nr = tags.get('addr:housenumber', None)
     street = tags.get('addr:street', None)
     if nr and street:
         return f'{street} {nr}'
 
+
+def to_tuple(tags):
+    nr = tags.get('addr:housenumber', None)
+    street = tags.get('addr:street', None)
+    if nr and street:
+        try:
+            nr = re.match(r'^\d+', nr)[0]
+        except TypeError:
+            pass
+        return (street, nr)
+
+
 buildings = []
 for building in result.ways:
+    addr = to_tuple(building.tags)
     b = {
         'id': building.id,
         'address': to_address(building.tags),
-        'entrances': {}
+        'entrances': {},
+        'addresses': [addr] if addr else []
     }
     buildings.append(b)
     for id in building._node_ids:
@@ -36,6 +53,13 @@ for building in result.ways:
         if not (entrance and address):
             continue
         b['entrances'].setdefault(address, []).append(id)
+        a_tuple = to_tuple(entrance.tags)
+        if a_tuple and (a_tuple not in b['addresses']):
+            b['addresses'].append(a_tuple)
 
+buildings.sort(
+    key=lambda b: len(b['addresses']) + 100 if len(set(a[0] for a in b['addresses'])) < len(b['addresses']) else 0,
+    reverse=True)
+for b in buildings:
+    del b['addresses']
 print(json.dumps(buildings, indent=1, ensure_ascii=False))
-
